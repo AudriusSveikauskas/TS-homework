e2c9d64316bffff498a7e37ceb1f8f70c4ef6d96
Audrius Sveikauskas <audrius@sveikauskas.dev>
2022-08-05 16:59:38 +0300

    chore: deleted sample task

--- lesson-4/src/enums.ts
+++ lesson-4/src/enums.ts
@@ -1,41 +1,5 @@
 /* eslint no-console: ["error", { allow: ["groupCollapsed", "log", "groupEnd", "dir", "group"] }] */
 
-/*
-  Enum(išvardinimas) - tai konstantų rinkinys.
-  Enum(išvardinimai) yra naudojami įvardinti tipams ar nustatymams.
-  Jų reikšmės dažniau naudojamos tipo patikrinimams, nei atvazdavimui.
-*/
-
-// Skaitiniai išvardinimai (numeric enums)
-// Nurodžius pirmajai konstantai reikšmę, sekančios susinumeruoja pradedant nurodyta reikšme
-enum WeekDays {
-  Monday = 1,
-  Tuesday, // 2
-  Wednesday, // 3
-  Thursday, // 4
-  Friday, // 5
-  Saturday, // 6
-  Sunday, // 7
-}
-
-// Jei nenurodote pirmosios reikšmės, numeruojama nuo 0
-enum Answer {
-  No, // 0
-  Yes, // 1
-}
-
-// Jei išvardinamų konstantų tipas yra ne skaičius, privalote įvardinti kiekvieną narį
-enum WeekDayNamesLT {
-  Monday = 'pirmadienis',
-  Tuesday = 'antradienis',
-  Wednesday = 'trečiadienis',
-  Thursday = 'ketvirtadienis',
-  Friday = 'penktadienis',
-  Saturday = 'šeštadienis',
-  Sunday = 'sekmadienis',
-}
-
-// Alikite užduotis, ir atspausdinkite savo sprendimo pavyzdžius užduoties blokuose
 console.group('Literal types - užduotys');
 {
   // ↓↓↓↓ Tipus ir užduotims bendrus kintamuosius aprašykite čia ↓↓↓↓
