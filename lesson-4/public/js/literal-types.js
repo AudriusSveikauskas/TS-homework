51f2580d4755e0ca5229061d8040cd90ccf19481
Audrius Sveikauskas <audrius@sveikauskas.dev>
2022-08-06 15:53:31 +0300

    chore: deleted sample task

--- lesson-4/public/js/literal-types.js
+++ lesson-4/public/js/literal-types.js
@@ -1,24 +1,4 @@
 "use strict";
-const country = 'Latvia';
-const country2 = 'Estonia';
-const req1 = {
-    url: 'https://tavo-veidas.lt',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    method: 'GET',
-};
-const req2 = {
-    url: 'https://tavo-veidas.lt/api/',
-    headers: {
-        'Authorization': 'Bearer fhdguisdf8g76sdf89ghf5..?sfg',
-        'Content-Type': 'application/json',
-    },
-    body: {
-        image: 'https://i.pinimg.com/474x/67/0a/1d/670a1d06e7bff426ec343e8c06c93ca5--crazy-faces-strange-people.jpg',
-    },
-    method: 'POST',
-};
 console.group('Literal types - užduotys');
 {
     console.group('1. Sukurkite konkrečius 5 dižiausių Vokietijos miestų tipus. Sukurkite tipą, kurio reikšmė būtų viena iš miestų.');
